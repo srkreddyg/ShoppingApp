1d80306236b845957e672942c0193f1b27b9959c
srkreddyg <101466810+srkreddyg@users.noreply.github.com>
2022-12-09 13:11:37 +0530

    Add project files.

---
+++ Shopping.Client/Data/ProductContext.cs
@@ -0,0 +1,60 @@
+﻿using Shopping.Client.Models;
+using System.Collections.Generic;
+
+namespace Shopping.Client.Data
+{
+    public static class ProductContext
+    {
+        public static readonly List<Product> Products = new List<Product>
+        {
+            new Product()
+                {
+                    Name = "IPhone X",
+                    Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    ImageFile = "product-1.png",
+
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Name = "Samsung 10",
+                    Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    ImageFile = "product-2.png",
+
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Name = "Huawei Plus",
+                    Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    ImageFile = "product-3.png",
+
+                    Category = "White Appliances"
+                },
+                new Product()
+                {
+                    Name = "Xiaomi Mi 9",
+                    Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    ImageFile = "product-4.png",
+
+                    Category = "White Appliances"
+                },
+                new Product()
+                {
+                    Name = "HTC U11+ Plus",
+                    Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    ImageFile = "product-5.png",
+
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Name = "LG G7 ThinQ EndofCourse",
+                    Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    ImageFile = "product-6.png",
+
+                    Category = "Home Kitchen"
+                }
+        };
+    }
+}
